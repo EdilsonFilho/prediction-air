171d29b5515906aa14f49e7a3ddbcd59cc3e76f6
ronaldaraujo <f.ronaldaraujo@gmail.com>
2020-08-17 18:46:02 -0300

    Melhorias

---
+++ app/Http/Controllers/Dashboard/SurveyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SurveyController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,14 @@ Route::group(['middleware' => ['auth'], 'namespace' => 'Dashboard', 'prefix' => 
     Route::put('image/{id}/upload', 'ImageController@upload')->name('image.upload');
     Route::delete('image/destroy/{file}', 'ImageController@destroy')->name('image.destroy');
 
-    // Route::resource('patients', 'PatientController');
     Route::post('patients', 'PatientController@store')->name('patients.store');
     Route::get('patients', 'PatientController@index')->name('patients.index');
     Route::get('patients/create', 'PatientController@create')->name('patients.create');
     Route::delete('patients/{user}', 'PatientController@destroy')->name('patients.destroy');
     Route::put('patients/{user}', 'PatientController@update')->name('patients.update');
     Route::get('patients/{user}/edit', 'PatientController@edit')->name('patients.edit');
+
+    Route::get('survey/{user}', 'SurveyController@index')->name('surveys.index');
 
     Route::group(['middleware' => ['admin']], function () {
         Route::resource('users', 'UserController');
